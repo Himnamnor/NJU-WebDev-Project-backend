43386006461f0e3ba36bf3c768bcfa8d191c9a1b
Zuxi Chen <3226584200@qq.com>
2025-07-19 22:26:40 +0800

    edit entityscan

--- src/main/java/com/nagisa/furukawa/FurukawaApplication.java
+++ src/main/java/com/nagisa/furukawa/FurukawaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.nagisa.furukawa.Repository")
-@EntityScan(basePackages = "com.nagisa.furukawa.Entity")
+@EntityScan(basePackages = "com.nagisa.furukawa.PO")
 public class FurukawaApplication {
 
 	public static void main(String[] args) {
